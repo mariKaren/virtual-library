98b0b124b879eaa47d9a4fe30ff568cee95326e4
mariKaren <karenbmari@gmail.com>
2025-04-20 10:37:17 +0200

    Modificacion del metodo register:role user unicamente

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -16,7 +16,6 @@ class AuthController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
-            'role' => 'nullable|in:user', 
         ]);
 
         if ($validator->fails()) {
@@ -30,7 +29,7 @@ class AuthController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'role' => $request->role ?? 'user',
+            'role' => 'user',
         ]);
 
         $token = $user->createToken('auth_token')->plainTextToken;
